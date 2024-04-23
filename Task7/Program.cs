e7762403396641fd02b8236487b55111c926a4da
Oleg Karlin <o.karlin1981@gmail.com>
2024-04-23 12:28:10 +0300

    add Sem1:Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,22 @@
+﻿// Написать программу, которая на вход принимает число (number), 
+// а на выходе выводит все чётные числа от 1 до number (включительно), 
+// после каждого числа должен быть знак пробела.
+
+Console.WriteLine("Введите положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int count = 1;
+
+    while (count <= number)
+    {
+        if (count % 2 == 0)
+            Console.Write(count + " ");
+        count++;
+    }
+}
+else
+{
+    Console.WriteLine("Число не является положительным!");
+}
